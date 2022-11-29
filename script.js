e2b23b123c7f2b64fde4e7b61326f3e700e22430
HollyLgru <H.grudovich@zollege.com>
2022-11-29 13:56:45 -0600

    getting there

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
-})
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+
+    document.getElementById('image')
+    .style.display = "none";
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+    // document.getElementById('image')
+    // .style.display = "block";
+  }
+
+  function showImage(e) {
+    const picture = document.querySelector(`img[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    document.getElementById('image')
+    .style.display = "block";
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+  window.addEventListener('keydown', showImage);
